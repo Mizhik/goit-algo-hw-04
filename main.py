0d3fdc091b200796256f18316fd5bbc24fedcd59
Mizhik <vlad.smilono2018@gmail.com>
2024-01-30 18:41:31 +0200

    first task

---
+++ main.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+path = Path("M:/vscPython/repository/goit-algo-hw-04/text.txt")
+
+def total_salary(path):
+    try:
+        with open(path, 'r', encoding="utf8") as file:
+            file = file.readlines()
+            outlist = []
+        for line in file:
+            line = line.split(',')
+            outlist.append(int(line[1]))    
+        all_s = sum(outlist)
+        average_sum = int(all_s/len(outlist))
+        result = (all_s,average_sum)
+        return result
+    except FileNotFoundError:
+        return print(f"{path} - not found")
+
+total, average = total_salary(path)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
